26b8c205eee582f9b7c60bfcecbbbca65ced4fa7
CAROLKOECH <koechc44@gmail.com>
2023-06-14 19:12:19 +0300

    Done with assignment~

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    if (customerName) {
+      customerName = customerName.toUpperCase();
+      return customerName;
+    } else {
+      return "customerName is not defined.";
+    }
+  }
+  var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  console.log(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';  // Update the value to 'maybe bob'
+    console.log(bestCustomer);
+  }
+  
+  const leastFavoriteCustomer = 'John Doe';
+
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
+
+
+
+  
+
+
+
